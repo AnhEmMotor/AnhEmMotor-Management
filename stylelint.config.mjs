c7e1d02e38f9bad20bce0b15652b4f53997d4bb6
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2026-06-23 09:34:44 +0700

    feat: debt management not using hard core data and connect to backend

---
+++ stylelint.config.mjs
@@ -0,0 +1,12 @@
+export default {
+	extends: [
+		"stylelint-config-standard",
+		"stylelint-config-html/vue",
+		"stylelint-config-recommended-scss",
+		"stylelint-config-recommended-vue",
+		"stylelint-config-recess-order",
+	],
+	rules: {
+		"no-empty-source": null,
+	},
+};
