c90c163edfc460d7e5048a2fb9a394eb72aca955
WorL-IT <nqv.quocviet@gmail.com>
2026-07-20 18:03:43 +0700

    refactor(contract): remove finance contract module

--- src/router/modules/contract.ts
+++ src/router/modules/contract.ts
@@ -48,34 +48,5 @@ export const contractRoutes: AppRouteRecord = {
         isHide: true,
       },
     },
-    {
-      path: "finance",
-      name: "FinanceContract",
-      component: "/Admin/view/contract/finance/index",
-      meta: {
-        title: "menus.contract.finance",
-        icon: "ri:bank-card-line",
-      },
-    },
-    {
-      path: "finance/preview/:id?",
-      name: "FinanceContractPreview",
-      component: "/Admin/view/contract/finance/finance-contract-detail",
-      meta: {
-        title: "Chi tiết Hợp đồng tài chính",
-        icon: "ri:file-search-line",
-        isHide: true,
-      },
-    },
-    {
-      path: "finance/save/:id?",
-      name: "FinanceContractSave",
-      component: "/Admin/view/contract/finance/save",
-      meta: {
-        title: "Tạo Hợp đồng tài chính",
-        icon: "ri:file-add-line",
-        isHide: true,
-      },
-    },
   ],
 };
