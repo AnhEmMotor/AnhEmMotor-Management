4ea9ac568d569d993fe53b902137b69b8a5ebd14
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2026-08-06 16:07:21 +0700

    feat: remove user guide in workspace

--- src/view/workspace.vue
+++ src/view/workspace.vue
@@ -209,15 +209,6 @@ const workspaces = computed(() => {
       hasAccess: moduleHasAccess(orderMenu, menuList),
       path: "/Order/management/draft",
     },
-    {
-      title: "Hướng dẫn sử dụng",
-      subtitle: "User Guide",
-      icon: markRaw(Document),
-      color: "#0284c7",
-      shadowColor: "rgba(2, 132, 199, 0.15)",
-      hasAccess: true,
-      path: "/manual",
-    },
   ].filter((workspace) => workspace.hasAccess);
 });
 </script>
